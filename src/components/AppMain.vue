8da5d52401682c0561b87577e605c051b507d860
Paparellav <vitantoniopaparella@gmail.com>
2022-05-31 14:44:02 +0200

    section 9 completed

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -30,6 +30,9 @@
     <section class="mt-5 mb-5">
       <AppSection8 />
     </section>
+    <section>
+      <AppSection9 :maps="mapArray" />
+    </section>
   </div>
 </template>
 
@@ -44,6 +47,7 @@ import AppJumbo2 from "./AppJumbo2.vue";
 import AppSection6 from "./AppSection6.vue";
 import AppSection7 from "./AppSection7.vue";
 import AppSection8 from "./AppSection8.vue";
+import AppSection9 from "./AppSection9.vue";
 
 export default {
   name: "AppMain",
@@ -58,6 +62,121 @@ export default {
     AppSection6,
     AppSection7,
     AppSection8,
+    AppSection9,
+  },
+  data: function () {
+    return {
+      mapArray: [
+        {
+          image: "vt (21).png",
+          name: "image-1",
+        },
+        {
+          image: "vt (17).png",
+          name: "image-2",
+        },
+        {
+          image: "vt (14).png",
+          name: "image-3",
+        },
+        {
+          image: "vt (4).png",
+          name: "image-4",
+        },
+        {
+          image: "vt (1).png",
+          name: "image-5",
+        },
+        {
+          image: "vt (3).png",
+          name: "image-6",
+        },
+        {
+          image: "vt (12).png",
+          name: "image-7",
+        },
+        {
+          image: "vt (18).png",
+          name: "image-8",
+        },
+        {
+          image: "vt (23).png",
+          name: "image-9",
+        },
+        {
+          image: "vt (24).png",
+          name: "image-10",
+        },
+        {
+          image: "vt (16).png",
+          name: "image-11",
+        },
+        {
+          image: "vt (13).png",
+          name: "image-12",
+        },
+        {
+          image: "vt.png",
+          name: "image-13",
+        },
+        {
+          image: "vt (2).png",
+          name: "image-14",
+        },
+        {
+          image: "vt (5).png",
+          name: "image-15",
+        },
+        {
+          image: "vt (10).png",
+          name: "image-16",
+        },
+        {
+          image: "vt (19).png",
+          name: "image-17",
+        },
+        {
+          image: "vt (25).png",
+          name: "image-18",
+        },
+        {
+          image: "vt (22).png",
+          name: "image-19",
+        },
+        {
+          image: "vt (15).png",
+          name: "image-20",
+        },
+        {
+          image: "vt (8).png",
+          name: "image-21",
+        },
+        {
+          image: "vt (9).png",
+          name: "image-22",
+        },
+        {
+          image: "vt (6).png",
+          name: "image-23",
+        },
+        {
+          image: "vt (7).png",
+          name: "image-24",
+        },
+        {
+          image: "vt (11).png",
+          name: "image-25",
+        },
+        {
+          image: "vt (20).png",
+          name: "image-26",
+        },
+        {
+          image: "vt (26).png",
+          name: "image-27",
+        },
+      ],
+    };
   },
 };
 </script>
---
+++ src/components/AppSection9.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="fluid-container">
+    <div class="map_card">
+      <img
+        v-for="(item, index) in maps"
+        :key="index"
+        :src="require(`../assets/img/${maps[index].image}`)"
+        alt=""
+      />
+      <div class="google_banner">
+        <img src="../assets/img/google4.png" alt="" />
+      </div>
+      <div class="maps_thumbs text-center">
+        <h1>+</h1>
+        <h1>-</h1>
+      </div>
+      <div class="directions_banner">
+        <img src="../assets/img/cattura.jpg" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppSection9",
+  props: {
+    maps: Array,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../style/common.scss";
+@import "../style/variables.scss";
+
+.map_card {
+  position: relative;
+  img {
+    width: calc(100% / 9);
+  }
+  .google_banner {
+    position: absolute;
+    bottom: 5%;
+    right: 48%;
+    img {
+      width: 120px;
+    }
+  }
+  .maps_thumbs {
+    position: absolute;
+    bottom: 3%;
+    right: 0.5%;
+    h1 {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 0.3rem 0.7rem;
+      font-size: 2rem;
+      margin: 0.2rem 0;
+      color: $tuatara;
+      background-color: $sugar-cane;
+      cursor: pointer;
+    }
+  }
+  .directions_banner {
+    position: absolute;
+    top: 3%;
+    left: 1%;
+    img {
+      width: 300px;
+    }
+  }
+}
+</style>
